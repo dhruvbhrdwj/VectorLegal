4931fdcd47944e394c4d4836441437f8b89daa7b
dhruv <dhruvbhardwaj3012@gmail.com>
2024-03-20 14:39:06 +0530

    Added the API, removed the data content and the embeddings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -4,7 +4,7 @@ from llama_index.core.settings import Settings
 
 
 def init_settings():
-    model = os.getenv("MODEL", "gpt-3.5-turbo")
+    model = os.getenv("MODEL", "gpt-4-0125-preview")
     Settings.llm = OpenAI(model=model)
     Settings.chunk_size = 1024
     Settings.chunk_overlap = 20
